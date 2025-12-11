f21d8cb83e53026ac75ba43350f83eee908ca95f
souvik <aimofsouvik14@gmail.com>
2025-12-11 23:27:37 +0530

    assigment - W2

--- DS/Find Minimum in Rotated Sorted Array II.java
+++ DS/Find Minimum in Rotated Sorted Array II.java
@@ -2,7 +2,17 @@ class Solution {
     public int findMin(int[] nums) {
         int l=0;
         int r= nums.length-1;
-
+/*
+If nums[mid] > nums[r], 
+the minimum element must be in the right half (mid+1 to r),
+ as the left half is sorted and nums[mid] 
+ is greater than nums[r], indicating a rotation point
+  after mid.
+If nums[mid] < nums[r], the minimum element is 
+in the left half (l to mid), including mid, because 
+the right part is sorted and nums[mid] is smaller than 
+nums[r].
+*/
         while(l<r){
             int mid=l+(r-l)/2;
             
