1e222c79148936f587846222b198a72bbbf4e25b
souvik <aimofsouvik14@gmail.com>
2025-12-15 20:09:31 +0530

    assignment - W3

---
+++ DS/sort_wave.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortInWave(int arr[]) {
+        // code here
+        int i=0;
+        int j=1;
+
+        while(j!=arr.length && i<arr.length){
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i+=2;
+            j = i+1;
+        }
+    }
+}
