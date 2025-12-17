6c67bb6a3d3deae798aad3bb84eaa26e56c09522
grace <aimofsouvik14@gmail.com>
2025-12-17 21:53:56 +0530

    assignment - W3

---
+++ DS/W3 - array+Strings/Sum of Subarrays.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int subarraySum(int[] arr) {
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i]*(i+1)*(n-i);
+
+        }
+        return sum;
+    }
+}
