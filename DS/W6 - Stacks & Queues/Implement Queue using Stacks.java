766c172ab26473b5e65080f022a64d5036b615df
grace <aimofsouvik14@gmail.com>
2026-01-18 23:46:42 +0530

    W6 - Assignment

--- DS/W6 - Stacks & Queues/Implement Queue using Stacks.java
+++ DS/W6 - Stacks & Queues/Implement Queue using Stacks.java
@@ -1,43 +1,41 @@
-import java.util.Stack;
-
 class MyQueue {
-    private Stack<Integer> input;
-    private Stack<Integer> output;
+    Stack<Integer> s1;
+    Stack<Integer> s2;
 
     public MyQueue() {
-        input = new Stack<>();
-        output = new Stack<>();
+        s1 = new Stack<>();
+        s2 = new Stack<>();
     }
 
-    // Push element x to the back of queue
     public void push(int x) {
-        input.push(x);
+        s1.push(x);
     }
 
-    // Removes the element from the front of queue and returns it
     public int pop() {
-        shiftStacks();
-        return output.pop();
-    }
-
-    // Get the front element
-    public int peek() {
-        shiftStacks();
-        return output.peek();
-    }
-
-    // Returns whether the queue is empty
-    public boolean empty() {
-        return input.isEmpty() && output.isEmpty();
-    }
-
-    // Helper method to move elements from input to output stack
-    private void shiftStacks() {
-        // Only shift if output stack is empty
-        if (output.isEmpty()) {
-            while (!input.isEmpty()) {
-                output.push(input.pop());
+        if(!s2.isEmpty()){
+            return s2.pop();
+        }else{
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
             }
+            return s2.pop();
         }
+
+    }
+
+    public int peek() {
+        if(!s2.isEmpty()){
+            return s2.peek();
+        }else{
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            return s2.peek();
+        }
+
+    }
+
+    public boolean empty() {
+        return s2.isEmpty() && s1.isEmpty();
     }
 }
