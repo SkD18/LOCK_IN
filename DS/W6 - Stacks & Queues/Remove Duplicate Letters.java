aadb818468167013e951c445b788c686726709eb
grace <aimofsouvik14@gmail.com>
2026-01-22 00:19:12 +0530

    W6 - Assignment

--- DS/W6 - Stacks & Queues/Remove Duplicate Letters.java
+++ DS/W6 - Stacks & Queues/Remove Duplicate Letters.java
@@ -24,4 +24,43 @@ class Solution {
         for (char c : stack) sb.append(c);
         return sb.toString();
     }
+    /***
+     * sliding window approach :
+     * class Solution {
+     *     public String removeDuplicateLetters(String s) {
+     *
+     *         int[] freq = new int[26];
+     *         boolean[] used = new boolean[26];
+     *
+     *         for (char c : s.toCharArray()) {
+     *             freq[c - 'a']++;
+     *         }
+     *
+     *         StringBuilder res = new StringBuilder();
+     *
+     *         for (char c : s.toCharArray()) {
+     *             freq[c - 'a']--;
+     *
+     *             if (used[c - 'a']) continue;
+     *
+     *             while (res.length() > 0) {
+     *                 char last = res.charAt(res.length() - 1);
+     *
+     *                 if (last > c && freq[last - 'a'] > 0) {
+     *                     used[last - 'a'] = false;
+     *                     res.deleteCharAt(res.length() - 1);
+     *                 } else {
+     *                     break;
+     *                 }
+     *             }
+     *
+     *             res.append(c);
+     *             used[c - 'a'] = true;
+     *         }
+     *
+     *         return res.toString();
+     *     }
+     * }
+     */
+    /
 }
